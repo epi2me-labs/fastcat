6247909ad6ceedafac2fc6ca04993d61c01650b4
Chris Wright <chris.wright@nanoporetech.com>
2021-07-06 14:17:29 +0100

    update CI scripts and bump version

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,7 @@
 
 NAME=fastcat
 
-gcc -o $NAME main.c -lz -lm
+gcc -o $NAME main.c args.c fastqcomments.c writer.c -lz -lm
 
 mkdir -p $PREFIX/bin
 cp $NAME $PREFIX/bin && chmod +x $PREFIX/bin/$NAME
