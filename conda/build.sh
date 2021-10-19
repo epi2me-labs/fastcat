78dc8925916f64d1a400a46af4cef39ea78bbd10
Chris Wright <chris.wright@nanoporetech.com>
2021-10-19 15:11:04 +0100

    Fix CI

--- conda/build.sh
+++ conda/build.sh
@@ -2,7 +2,7 @@
 
 NAME=fastcat
 
-gcc -o $NAME main.c args.c fastqcomments.c writer.c -lz -lm
+gcc -I src -o $NAME src/main.c src/args.c src/fastqcomments.c src/writer.c -lz -lm
 
 mkdir -p $PREFIX/bin
 cp $NAME $PREFIX/bin && chmod +x $PREFIX/bin/$NAME
